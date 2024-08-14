debc467ffdc59a2d4a80e19186caeb7539cbcf44
verenciakj <verencia.john@kanaan.sch.id>
2024-08-14 14:31:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector('body').style.backgroundColor="gray";
+document.querySelector('body').style.display="flex";
+document.querySelector('body').style.flexDirection="column";
+document.quer            ySelector('body').style.minHeight="100vh";
+document.querySelector('body').style.justifyContent="center";
+document.querySelector('body').style.alignItems="center";
+
+let tagname = document.getElementById('tagname');
+tagname.innerHTML="Verencia KJ <br> XI A";
+tagname.style.color="white";
+tagname.style.fontSize="3rem";
+tagname.style.textAlign="center";
+tagname.style.border="0.2em solid black";
+tagname.style.padding="1em";
+
